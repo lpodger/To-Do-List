fa344d8f21350d592530340ef80e88c4acdfd145
Lachlan Podger <114342815+lpodger@users.noreply.github.com>
2022-11-06 20:19:32 +0800

    Add files via upload

---
+++ js/tasks2.js
@@ -0,0 +1,47 @@
+// tasks.js #2 
+// This script manages a to-do list.
+
+// Need a global variable:
+var tasks = []; 
+
+// Function called when the form is submitted.
+// Function adds a task to the global array.
+function addTask() {
+    'use strict';
+
+    // Get the task:
+    var task = document.getElementById('task');
+
+    // Reference to where the output goes:
+    var output = document.getElementById('output');
+    
+    // For the output:
+    var message = '';
+
+    if (task.value) {
+    
+        // Add the item to the array:
+        tasks.push(task.value);
+		task.value = '';
+        
+        // Update the page:
+        message = '<h2>To-Do</h2>';
+        message +=  '<ol><li>'; 
+		message +=  tasks.join('</li><li>'); 
+		message +=  '</li></ol>';
+		
+        output.innerHTML = message;
+        
+    } // End of task.value IF.
+
+    // Return false to prevent submission:
+    return false;
+    
+} // End of addTask() function.
+
+// Initial setup:
+function init() {
+    'use strict';
+    document.getElementById('theForm').onsubmit = addTask;
+} // End of init() function.
+window.onload = init;
